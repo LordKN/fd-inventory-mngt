e225d543c08a8eb190623b6ba380bffb980f6afc
Nguyen <mickey171904@gmail.com>
2026-06-05 15:33:32 +0700

    firebase-config.js done
    for user login

---
+++ public/js/firebase/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0fhRsOKrdK9D3bge1hqsGQqy0uJIa6qQ",
+  authDomain: "fd-inspection-system.firebaseapp.com",
+  projectId: "fd-inspection-system",
+  storageBucket: "fd-inspection-system.firebasestorage.app",
+  messagingSenderId: "275179080176",
+  appId: "1:275179080176:web:1ff7aea33e12e51b0e7535",
+  measurementId: "G-2WEVMX3EN4",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
