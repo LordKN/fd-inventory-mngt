f9d21a825f8a7f0fcec0fcb7aaee696081a6febb
Nguyen <mickey171904@gmail.com>
2026-06-08 15:05:32 +0700

    Move everything to public folder
    Since we are only serve public

---
+++ public/js/pages/dashboard.js
@@ -0,0 +1,24 @@
+import { auth } from "../firebase/auth.js";
+
+import {
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+const userInfo = document.getElementById("userInfo");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  userInfo.textContent = `Xin chào, ${user.email}`;
+});
+
+logoutBtn.addEventListener("click", async() => {
+    await signOut(auth);
+
+    window.location.href = "index.html";
+})
