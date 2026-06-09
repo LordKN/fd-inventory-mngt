6965141c2915cf6d606c418b39e0b2287c0dc71a
Nguyen <mickey171904@gmail.com>
2026-06-09 16:35:53 +0700

    Save data on cloud

--- public/js/pages/daily-check.js
+++ public/js/pages/daily-check.js
@@ -1,6 +1,6 @@
 import { auth } from "../firebase/auth.js";
 import { db } from "../firebase/firestore.js";
-import { storage } from "../firebase/storage.js";
+//import { storage } from "../firebase/storage.js";
 
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 
@@ -10,12 +10,13 @@ import {
   serverTimestamp,
 } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
+/*
 import {
   ref,
   uploadBytes,
   getDownloadURL,
 } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-storage.js";
-
+*/
 const form = document.getElementById("dailyCheckForm");
 
 let currentUser = null;
@@ -31,35 +32,39 @@ onAuthStateChanged(auth, (user) => {
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const vehicle = document.getElementById("vehicle").value;
-  const status = document.getElementById("status").value;
-  const note = document.getElementById("note").value;
-  const photoFile = document.getElementById("photo").files[0];
+  try {
+    const vehicle = document.getElementById("vehicle").value;
+    const status = document.getElementById("status").value;
+    const note = document.getElementById("notes").value;
+    const photoFile = document.getElementById("photo").files[0];
 
-  let photoURL = null;
+    let photoURL = null;
 
-  if (photoFile) {
-    const photoRef = ref(
-      storage,
-      `daily-check-photos/${Date.now()}-${photoFile.name}`,
-    );
+    /*
+    if (photoFile) {
+      const photoRef = ref(
+        storage,
+        `daily-check-photos/${Date.now()}-${photoFile.name}`,
+      );
 
-    await uploadBytes(photoRef, photoFile);
-    photoURL = await getDownloadURL(photoRef);
+      await uploadBytes(photoRef, photoFile);
+      photoURL = await getDownloadURL(photoRef);
+    }
+    */
+    await addDoc(collection(db, "daily-checks"), {
+      vehicle,
+      status,
+      note,
+      photoURL,
+      createdAt: serverTimestamp(),
+      date: new Date().toISOString().split("T")[0],
+      createdBy: currentUser.email,
+    });
+
+    alert("Báo cáo đã được lưu thành công!");
+    form.reset();
+  } catch (error) {
+    console.error("Save report error:", error);
+    alert("Lỗi khi lưu báo cáo.");
   }
-
-  await addDoc(collection(db, "daily-checks"), {
-    vehicle,
-    status,
-    note,
-    photoURL,
-
-    createdAt: serverTimestamp(),
-    date: new Date().toISOString().split("T")[0],
-
-    createdBy: currentUser.email,
-  });
-
-  alert("Báo cáo đã được lưu thành công!");
-  form.reset();
 });
